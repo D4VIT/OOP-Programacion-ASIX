5173db611cd09b318162c8d3e606fd57a4fed7f6
Joan David <tjoan9067@gmail.com>
2024-04-18 09:41:48 +0200

    codigo profe

--- Calculadora/Principal.java
+++ Calculadora/Principal.java
@@ -4,5 +4,7 @@ class Principal
 {
     public static void main(String[] args) {
         Pantalla.escribir("Hola mundo");
+        String variable Teclado.leer("Escribe algo por aqui");
+        Pantalla.escribir(variable);
     }
 }
