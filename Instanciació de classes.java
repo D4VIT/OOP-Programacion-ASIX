f26cdd25af839f9e1e87af832b567725e726650d
Joan David <tjoan9067@gmail.com>
2024-04-25 09:30:46 +0200

    ahora imprime por pantalla los valores primari y secundari

--- Instanciació de classes.java
+++ Instanciació de classes.java
@@ -28,4 +28,8 @@ public class ClasseA {
         ClasseA a = new ClasseA();
         ClasseA b = new ClasseA(20);
         ClasseA c = new ClasseA(20, 40);
+        System.out.println("L'objecte _a:_ conté: " + a.getPrimari() + ", " + a.getSecundari());
+        System.out.println("L'objecte _b:_ conté: " + b.getPrimari() + ", " + b.getSecundari());
+        System.out.println("L'objecte _c:_ conté: " + c.getPrimari() + ", " + c.getSecundari());
+    }
 }
