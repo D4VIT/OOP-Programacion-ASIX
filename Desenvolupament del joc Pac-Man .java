a4c93500c7b868e5b75c6b1ab23e04fb981623c7
Joan David <tjoan9067@gmail.com>
2024-04-26 12:28:34 +0200

    creamos la clase fantasma con sus atributos y constructor y despues creamos el metodo para mover al fantasma a una nueva posicion

--- Desenvolupament del joc Pac-Man .java
+++ Desenvolupament del joc Pac-Man .java
@@ -51,3 +51,17 @@ class PacMan {
         return puntuacio;
     }
 }
+
+class Fantasma {
+    private int posX;
+    private int posY;
+    private String nom;
+
+    public Fantasma(int posX, int posY, String nom) {
+        this.posX = posX;
+        this.posY = posY;
+        this.nom = nom;
+    }
+
+    
+}
