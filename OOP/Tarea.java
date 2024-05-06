cd50741f3968687843b276e04134f796bdd61c79
Joan David <tjoan9067@gmail.com>
2024-05-06 13:25:14 +0200

    creamos la clase tarea para representar una tarea

--- OOP/Tarea.java
+++ OOP/Tarea.java
@@ -1,1 +1,9 @@
+public class Tarea {
+    private String descripcion;
+    private String estado;
 
+    public Tarea(String descripcion, String estado) {
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+}
