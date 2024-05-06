e35b49f6d2a085a0b6711187b86bb783ef0b63f9
Joan David <tjoan9067@gmail.com>
2024-05-06 12:39:34 +0200

    creamos el constructor de media con sus atributos correspondientes

--- Codificació de classes.java
+++ Codificació de classes.java
@@ -2,4 +2,12 @@ public class Media {
     private String nom;
     private String autor;
     private int duradaSegons;
+
+    public Media(String nom, String autor, int duradaSegons) {
+        this.nom = nom;
+        this.autor = autor;
+        this.duradaSegons = duradaSegons;
+    }
+
+    
 }
