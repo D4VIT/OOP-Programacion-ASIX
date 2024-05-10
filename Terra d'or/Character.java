91ce8cfda4a8615724dcb4d54e74e5ffc1113f67
Joan David <tjoan9067@gmail.com>
2024-05-10 08:39:08 +0200

    creamos la clase caracteres con sus atributos

---
+++ Terra d'or/Character.java
@@ -0,0 +1,10 @@
+package Adventuregame;
+
+public class Character {
+    private String name;
+    private int level;
+    private int healthPoints;
+    private int manaPoints;
+    private String weapon;
+    private String armor;
+}
