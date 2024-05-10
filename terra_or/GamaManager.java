c99294ce1eead54494952e6ac998d1a5959f01b9
Joan David <tjoan9067@gmail.com>
2024-05-10 09:45:30 +0200

    creamos los apartados que se enseñaran por pantalla

--- terra_or/GamaManager.java
+++ terra_or/GamaManager.java
@@ -8,7 +8,17 @@ public class GamaManager {
         List<GameElement<Character>> characters = loadElementsFromXML("personatges.xml");
 
         for (GameElement<Character> characterElement : characters) {
-            Character
+            Character character = characterElement.getValue();
+
+            System.out.println("Nombre: " + character.getName());
+            System.out.println("Nivel: " + character.getLevel());
+            System.out.println("Puntos de vida: " + character.getHealthPoints());
+            System.out.println("Puntos de maná: " + character.getManaPoints());
+            System.out.println("Arma: " + character.getWeapon());
+            System.out.println("Armadura: " + character.getArmor());
+            System.out.println();
         }
     }
+
+    
 }
