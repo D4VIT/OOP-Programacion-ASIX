21bd1f9cd9e894008d6dff11ef4edfd86b90fc83
Joan David <tjoan9067@gmail.com>
2024-05-10 09:21:08 +0200

    creamos el constructor y sus atributos

--- terra_or/GameElement.java
+++ terra_or/GameElement.java
@@ -4,5 +4,20 @@ public class GameElement<T> {
     private String name;
     private T value;
 
-    
+    public GameElement(String name, T value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this
+    }
 }
