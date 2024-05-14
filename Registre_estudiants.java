6650555d8e36e4a184f9e99bb4fa41a7adf5cec1
Joan David <tjoan9067@gmail.com>
2024-05-14 10:59:34 +0200

    creamos la clase Estudiant con sus atributos

--- Registre_estudiants.java
+++ Registre_estudiants.java
@@ -1,1 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
 
+class Estudiant {
+    private String nom;
+    private String cognom;
+    private int edat;
+    private List<String> matèries;
+
+    public Estudiant(String nom, String cognom, int edat) {
+        this.nom = nom;
+        this.cognom = cognom;
+        this.edat = edat;
+        this.matèries = new ArrayList<>();
+    }
