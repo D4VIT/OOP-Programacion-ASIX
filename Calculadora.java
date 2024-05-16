98afa94fa7d1c0071f8dc9fd7a862db899ed28f6
Joan David <tjoan9067@gmail.com>
2024-05-16 08:25:20 +0200

    codigo del profe

--- Calculadora.java
+++ Calculadora.java
@@ -1,1 +1,13 @@
+import javax.swing.*;
 
+class Finestra {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Finestra d'exemple");
+        JButton button = new JButton("Clic aqui");
+
+        frame.add(button);
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
