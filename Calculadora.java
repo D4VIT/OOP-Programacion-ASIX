8f2816934fe7a93d0d5e6dfbe9d37a22f85e8662
Joan David <tjoan9067@gmail.com>
2024-05-16 08:41:41 +0200

    codigo del profe

--- Calculadora.java
+++ Calculadora.java
@@ -2,10 +2,19 @@ import javax.swing.*;
 
 class Finestra {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Finestra d'exemple");
-        JButton button = new JButton("Clic aqui");
+        JFrame frame = new JFrame("Calculadora");
+        JTextField pantallita = new JTextField();
+        JButton[] numericButtons = new JButton[10];
 
-        frame.add(button);
+        for (int i = 0; i < numericButtons.length; i++) {
+            numericButtons[i] = new JButton(Integer.toString(i));
+        }
+        pantallita.setEditable(false);
+
+        frame.add(pantallita);
+        for (JButton jButton : numericButtons) {
+            frame.add(jButton);
+        }
         frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
