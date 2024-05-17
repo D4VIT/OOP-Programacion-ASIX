d448c61acc18fc7f60ef725b71ef66978bd273de
Joan David <tjoan9067@gmail.com>
2024-05-17 09:11:12 +0200

    creamos la clase Tasca y sus atributos correspondientes

--- Gestor_tasques.java
+++ Gestor_tasques.java
@@ -2,3 +2,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
+
+class Task {
+    private String title;
+    private String description;
+    private String deadLine;
+    private String status;
+}
